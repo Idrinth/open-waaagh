7e8b3112a29ff26941c740d669238b98596e3b44
Björn Büttner <idrinth@users.noreply.github.com>
2023-02-24 01:05:35 +0100

    add missing autoloading

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use De\Idrinth\WAAAGHde\Home;
 use De\Idrinth\WAAAGHde\Post;
 use Twig\Loader\FilesystemLoader;
 
+require dirname(__DIR__) . '/vendor/autoload.php';
+
 (new Application())
     ->register(new PDO('mysql:host=' . $_ENV['DATABASE_HOST'] . ';dbname=' . $_ENV['DATABASE_DATABASE'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']))
     ->register(new FilesystemLoader(dirname(__DIR__) . '/templates'))
